51c1940f897424d0fac8c541035dd50fee3404c6
gebeto <slavik.nychkalo@gmail.com>
2020-01-25 22:33:40 +0200

    add 192 icon

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -23,6 +23,11 @@ app.get('/manifest.json', function(req, res) {
         "src": "/static/icon@512.png",
         "type": "image/png",
         "sizes": "512x512"
+      },
+      {
+        "src": "/static/icon@192.png",
+        "type": "image/png",
+        "sizes": "192x192"
       }
     ],
     "start_url": "/",
