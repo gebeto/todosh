129ddf1e6a09bfad52c0caf7aaf342c56279793c
gebeto <slavik.nychkalo@gmail.com>
2021-02-27 18:34:27 +0200

    Added header settings list

--- packages/app/src/components/Header/index.tsx
+++ packages/app/src/components/Header/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import './styles.scss';
 
+import { Modal } from '@wsl/shared/components/Modal';
+import { List } from '@wsl/shared/components/List';
+
 import { useRefresh } from '../../pages/Shopping/RefreshContext';
 import logoutIcon from 'url:./settings-icon.svg';
 
 
 export const Header: React.FC = () => {
+	const [open, setOpen] = React.useState(false);
 	const refresh = useRefresh();
 
 	const handleLogout = React.useCallback(() => {
@@ -13,16 +17,31 @@ export const Header: React.FC = () => {
 		window.location.reload();
 	}, []);
 
-	const handleRefresh = () => {
-		refresh();
+	const handleOpen = () => {
+		setOpen(true);
+	}
+
+	const handleClose = () => {
+		setOpen(false);
+	}
+
+	const onItemSelect = (item: any) => {
+		handleClose();
+		item.callback?.();
 	}
 
 	return (
 		<div className="header">
 			<div className="container header-container">
-				<h1 onClick={handleRefresh}>List</h1>
-				<img src={logoutIcon} height="26" onClick={handleLogout} />
+				<h1>List</h1>
+				<img src={logoutIcon} height="26" onClick={handleOpen} />
 			</div>
+			<Modal open={open} handleClose={handleClose}>
+				<List onItemSelect={onItemSelect} items={[
+					{ id: 1, title: "Refresh", callback: refresh },
+					{ id: 2, title: "Logout", callback: handleLogout},
+				]} />
+			</Modal>
 		</div>
 	)
 }
