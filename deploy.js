9297c0787e2419eaa5970f89ce64054d10457b58
gebeto <slavik.nychkalo@gmail.com>
2021-02-27 19:22:34 +0200

    Added deployment to the new todosh.github.io. Added storybook and app deployment at once. Added workspace build storybook and app by one command.

--- deploy.js
+++ deploy.js
@@ -1,3 +1,18 @@
 const ghpages = require('gh-pages');
 
-ghpages.publish('packages/app/dist', { history: false });
+
+const deploy = async () => {
+	// App
+	await ghpages.publish('packages/app/dist', {
+		history: false,
+		repo: 'https://github.com/todosh/todosh.github.io',
+	});
+	
+	// Storybook
+	await ghpages.publish('packages/shared/dist', {
+		dest: '/docs',
+		repo: 'https://github.com/todosh/todosh.github.io',
+	});
+}
+
+deploy();
