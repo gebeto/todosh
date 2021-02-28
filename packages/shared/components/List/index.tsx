3538929a4f6e60ef26ba77ad42b38487ed2ee6a0
gebeto <slavik.nychkalo@gmail.com>
2021-02-28 14:47:12 +0200

    Made list and list item generic components

--- packages/shared/components/List/index.tsx
+++ packages/shared/components/List/index.tsx
@@ -3,17 +3,33 @@ import * as React from 'react';
 import './styles.scss';
 
 
-export const ListItem: React.FC<any> = ({ item, titleKey, onItemSelect }) => (
-	<li
-		className="wsl-list-item"
-		onClick={onItemSelect ? () => onItemSelect(item) : onItemSelect}
-	>
-		{item[titleKey]}
-	</li>
-);
+export type ListItemProps<T> = {
+	item: T;
+	titleKey: keyof T;
+	onItemSelect: (item: T) => void;
+};
 
 
-export const List: React.FC<any> = ({ items, onItemSelect, titleKey="title" }) => {
+export function ListItem<T>({ item, titleKey, onItemSelect }: ListItemProps<T>) {
+	return (
+		<li
+			className="wsl-list-item"
+			onClick={onItemSelect ? () => onItemSelect(item) : onItemSelect}
+		>
+			{item[titleKey]}
+		</li>
+	);
+}
+
+
+export type ListProps<T> = {
+	items: Array<T>;
+	onItemSelect: (item: T) => void;
+	titleKey: keyof T;
+};
+
+
+export function List<T>({ items, onItemSelect, titleKey }: ListProps<T>) {
 	return (
 		<ul className="wsl-list">
 			{items.map((item: any) =>
@@ -22,3 +38,7 @@ export const List: React.FC<any> = ({ items, onItemSelect, titleKey="title" }) =
 		</ul>
 	);
 };
+
+List.defaultProps = {
+	titleKey: "title",
+}
